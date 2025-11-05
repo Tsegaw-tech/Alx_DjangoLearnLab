7b05b5be781da172b4b8af50e098208cca6224b8
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-11-05 16:20:29 +0300

    Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,7 +14,9 @@ def list_books_in_library(library_name):
     return library.books.all()
 
 
+
 def get_librarian_for_library(library_name):
-    """Retrieve the librarian for a library"""
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = Librarian.objects.get(library=library)
+    return librarian
+
