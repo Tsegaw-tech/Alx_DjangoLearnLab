89882b413f09190e00085fff4be7171f3403e71e
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-11-05 16:18:00 +0300

    Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,20 @@
+# relationship_app/query_samples.py
 from relationship_app.models import Author, Book, Library, Librarian
-from relationship_app.query_samples import *
 
-# Create data
-a1 = Author.objects.create(name="Jane Doe")
-b1 = Book.objects.create(title="Python Basics", author=a1)
-b2 = Book.objects.create(title="Advanced Django", author=a1)
-lib = Library.objects.create(name="Central Library")
-lib.books.set([b1, b2])
-Librarian.objects.create(name="John Smith", library=lib)
 
-# Test queries
-print(query_books_by_author("Jane Doe"))
-print(list_books_in_library("Central Library"))
-print(get_librarian_for_library("Central Library"))
+def query_books_by_author(author_name):
+    """Query all books by a specific author"""
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+
+def list_books_in_library(library_name):
+    """List all books in a library"""
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+def get_librarian_for_library(library_name):
+    """Retrieve the librarian for a library"""
+    library = Library.objects.get(name=library_name)
+    return library.librarian
