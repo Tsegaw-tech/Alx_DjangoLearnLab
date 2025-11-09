44bdcf636c4db36af52af89e8f7f61f86cfa75cb
unknown <tsegaw.grace@gmail.com>
2025-11-09 11:03:58 -0800

    Django Views and URL Configuration2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Library
+from django.views import View
+
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -8,3 +11,12 @@ def list_books(request):
 def library_detail(request, pk):
     library = get_object_or_404(Library, pk=pk)
     return render(request, 'relationship_app/library_detail.html', {'library': library})
+
+
+class LibraryDetailView(View):
+    template_name = 'relationship_app/library_detail.html'
+
+    def get(self, request, pk):
+        library = get_object_or_404(Library, pk=pk)
+        return render(request, self.template_name, {'library': library})
+
