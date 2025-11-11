b898a39500373509b3004c742fafafb1f1bde276
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-11-11 10:48:35 +0300

    modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-<<<<<<< HEAD
-from .models import Book, Library
-from django.views import View
-from django.views.generic import DetailView
-
-
-=======
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
->>>>>>> 2cc6c9f (Django Views and URL Configuration3)
 
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 def library_detail(request, pk):
     library = get_object_or_404(Library, pk=pk)
