ac4b9e2ff719d79d931fed898f1b7eeae6cf4a0c
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-11-11 12:08:25 +0300

    modify url

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
