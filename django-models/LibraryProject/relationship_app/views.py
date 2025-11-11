930c0754ed2cf19590e122adfc002bf5c9510bf0
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-11-11 10:35:43 +0300

    Django Views and URL Configuration3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+<<<<<<< HEAD
 from .models import Book, Library
 from django.views import View
 from django.views.generic import DetailView
 
 
+=======
+from django.views.generic import DetailView
+from .models import Book
+from .models import Library
+>>>>>>> 2cc6c9f (Django Views and URL Configuration3)
 
 
 def list_books(request):
