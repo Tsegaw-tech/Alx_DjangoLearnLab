28d68724c3e4c68715b582a89b7dddb8c572f0cc
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-11-19 16:52:43 +0300

    Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 
 
@@ -13,3 +15,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] 
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
     path('api/', include('api.urls')),  # Connects api app
 ]
+
 
