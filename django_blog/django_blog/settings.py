408602830ee9c5d3895b6fe35ff7553325159f2c
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-12-03 12:53:03 +0300

    Project Configuration for a Django Blog2

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -65,11 +65,16 @@ TEMPLATES = [
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_blog_db",       # your PostgreSQL database name
+        "USER": "postgres",              # your PostgreSQL username
+        "PASSWORD": "password",# your PostgreSQL password
+        "HOST": "127.0.0.1",             # usually localhost
+        "PORT": "5432",                  # default PostgreSQL port
     }
 }
+
 
 
 
@@ -107,12 +112,7 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
